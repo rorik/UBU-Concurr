cacb904fff08ea18b0197d85ede6df04aed921af
apm1005 <apm1005@alu.ubu.es>
2019-03-18 10:16:32 +0100

    Fixed previous mistakes made by @rorik

--- src/pg03/c02/SistemaLanzador.java
+++ src/pg03/c02/SistemaLanzador.java
@@ -1,11 +1,10 @@
 package pg03.c02;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import pg03.c01.ActividadEntradaPuerta;
 import pg03.c01.IParque;
 import pg03.c01.Parque;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class SistemaLanzador {
 	final static private int N = 5;
@@ -14,7 +13,7 @@ public class SistemaLanzador {
 		final IParque parque = new Parque();
 		ExecutorService executor = Executors.newCachedThreadPool();
 		for (int i = 0; i < N; i++) {
-			executor.submit(new ActividadEntradaPuerta(Character.toString('A' + i), parque));
+			executor.submit(new ActividadEntradaPuerta(Character.toString((char) ('A' + i)), parque));
 		}
 	}
 }
