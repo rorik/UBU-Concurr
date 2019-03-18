58eb109c4288d60de859844347fb7e9d7215b938
Roderick Diaz <about@rorik.me>
2019-03-18 10:08:48 +0100

    Added  p03 c02

---
+++ src/pg03/c02/SistemaLanzador.java
@@ -0,0 +1,20 @@
+package pg03.c02;
+
+import pg03.c01.ActividadEntradaPuerta;
+import pg03.c01.IParque;
+import pg03.c01.Parque;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SistemaLanzador {
+	final static private int N = 5;
+	
+	public static void main (String args[]) {
+		final IParque parque = new Parque();
+		ExecutorService executor = Executors.newCachedThreadPool();
+		for (int i = 0; i < N; i++) {
+			executor.submit(new ActividadEntradaPuerta(Character.toString('A' + i), parque));
+		}
+	}
+}
